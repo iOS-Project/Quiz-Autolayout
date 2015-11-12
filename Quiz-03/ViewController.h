38000a46e723e002f1c53cb4b1f7394cf7537f00
Lun Sovathana <lunsovathana@luns-mac-mini.local>
2015-11-12 14:01:16 +0700

    Initial Commit

---
+++ Quiz-03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz-03
+//
+//  Created by Lun Sovathana on 11/12/15.
+//  Copyright © 2015 Lun Sovathana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Quiz-03/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Quiz-03
+//
+//  Created by Lun Sovathana on 11/12/15.
+//  Copyright © 2015 Lun Sovathana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
